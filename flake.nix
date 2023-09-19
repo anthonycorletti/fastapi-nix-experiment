6d765fd79f2276fe6c6b1879c84e35ab12fc7398
Anthony Corletti <anthcor@gmail.com>
2023-09-19 16:50:05 -0400

    gitignore

--- flake.nix
+++ flake.nix
@@ -9,44 +9,31 @@
 
   outputs = { self, nixpkgs, mach-nix, flake-utils, ... }:
     let
-      pythonVersion = "python311";
+      pythonVersion = "python310";
     in
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         mach = mach-nix.lib.${system};
 
-        pythonApp = mach.buildPythonApplication ./.;
-        pythonAppEnv = mach.mkPython {
+        pythonAppEnv = mach.mkPython rec {
           python = pythonVersion;
           requirements = builtins.readFile ./requirements.txt;
         };
-        pythonAppImage = pkgs.dockerTools.buildLayeredImage {
-          name = pythonApp.pname;
-          contents = [ pythonApp ];
-          config.Cmd = [ "${pythonApp}/bin/main" ];
-        };
-      in
-      rec
-      {
-        packages = {
-          image = pythonAppImage;
-          pythonPkg = pythonApp;
-          default = packages.pythonPkg;
-        };
 
-        apps.default = {
-          type = "app";
-          program = "${packages.pythonPkg}/bin/main";
-        };
-
-        devShells.default = pkgs.mkShellNoCC {
-          packages = [ pythonAppEnv ];
+        devShell = pkgs.mkShell rec {
+          buildInputs = [
+            pythonAppEnv
+          ];
 
           shellHook = ''
             export PYTHONPATH="${pythonAppEnv}/bin/python"
           '';
         };
+
+        defaultPackage = pythonAppEnv;
+      in {
+        inherit devShell defaultPackage;
       }
     );
 }
