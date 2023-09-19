2cf583eb8ef7798b3db18276508b52c354fbab8c
Anthony Corletti <anthcor@gmail.com>
2023-09-19 16:13:45 -0400

    test

--- flake.nix
+++ flake.nix
@@ -43,9 +43,6 @@
           myPython
         ];
       };
-
-      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
     }
   );
 }
